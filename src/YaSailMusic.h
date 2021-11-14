d16e72baeff074ca9066ef4e65acce088babab5a
seejinmax <123712248+seejinmax@users.noreply.github.com>
2021-11-14 05:38:54 +0000

    Added template

---
+++ src/YaSailMusic.h
@@ -0,0 +1,17 @@
+
+#ifndef YASAILMUSIC_H
+#define YASAILMUSIC_H
+
+#include "track.h"
+
+//#include "playlistpage.h"
+
+
+//MainPage mainPage;
+//MainWindow mainWindoww;
+//Authorization *auth;
+
+struct BaseValues
+{
+    BaseValues();
+    QList<Track*> currentPlaylist;
