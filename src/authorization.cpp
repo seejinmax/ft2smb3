b9c8b11810e9e0b6b432aaffd3fb9831f376b19c
seejinmax <123712248+seejinmax@users.noreply.github.com>
2022-03-04 22:34:18 +0000

    Style nit.

--- src/authorization.cpp
+++ src/authorization.cpp
@@ -44,4 +44,4 @@ void Authorization::doAuth(QString username, QString password)
     r.setHeader(QNetworkRequest::ContentTypeHeader, "application/x-www-form-urlencoded");
     setupRequest(&r);
 
- 
+    QNetworkAccessManager* m = new QNetworkAcce
