0159e128b2f9952640a8c8f21f8db7042db37c01
seejinmax <123712248+seejinmax@users.noreply.github.com>
2022-07-01 13:15:47 +0000

    Don't need this

--- src/cacher.cpp
+++ src/cacher.cpp
@@ -53,4 +53,4 @@ void Cacher::getDownloadInfoFinished(const QJsonValue &value)
     QString downloadInfoUrl;
 
     foreach (const QJsonValue & value, qja) {
-  
+        if(value.toObject()["codec"].toString() == "
