3e955b3ae9eaf60fdb522ebddf0d0f19b78f05ce
seejinmax <123712248+seejinmax@users.noreply.github.com>
2022-10-08 14:20:30 +0000

    video/logo: prevent use of logos after they have been freed

--- src/models/playlistmodel.cpp
+++ src/models/playlistmodel.cpp
@@ -39,3 +39,75 @@ PlaylistModel::PlaylistModel(QObject *parent)
     m_hash.insert(Qt::UserRole+9 ,QByteArray("duration"));
     m_hash.insert(Qt::UserRole+10 ,QByteArray("storageDir"));
     m_hash.insert(Qt::UserRole+11 ,QByteArray("liked"));
+    m_hash.insert(Qt::UserRole+12 ,QByteArray("fileUrl"));
+   // PlaylistModel::model = this;
+  // qmlRegisterType<QList<Track*>>("org.ilyavysotsky.yasailmusic",1,0,"QList<Track*>");
+    m_api = new ApiRequest();
+    baseValues_->currentPlaylist=m_playList;
+}
+
+
+int PlaylistModel::rowCount(const QModelIndex &parent) const {
+    Q_UNUSED(parent)
+    return m_playList.size();
+}
+
+void PlaylistModel::setNewData() {
+ /*   beginRemoveRows(QModelIndex(), 0, m_playList.size()-1);
+
+    m_playList.clear();
+
+    endRemoveRows();
+        beginInsertRows(QModelIndex(), 0, new_playList.size()-1);
+           for (int i = 0; i<new_playList.size(); i++) {
+               qDebug() << "data:" << new_playList.at(i)->trackName << "\n";
+               m_playList.append(new_playList.at(i));
+
+}
+
+        endInsertRows();*/
+    m_playList =  baseValues_->currentPlaylist;
+
+}
+
+inline void delayy(int millisecondsWait)
+{
+    QEventLoop loop;
+    QTimer t;
+    t.connect(&t, &QTimer::timeout, &loop, &QEventLoop::quit);
+    t.start(millisecondsWait);
+    loop.exec();
+}
+
+
+
+QVariant PlaylistModel::data(const QModelIndex &index, int role) const {
+    Q_UNUSED(role);
+    if (!index.isValid())
+        return QVariant();
+
+    if (index.row() >= m_playList.size())
+        return QVariant();
+
+    Track* item = m_playList.at(index.row());
+    if(role == Qt::UserRole) {
+        return item->trackId;
+    } else if(role == Qt::UserRole+1) {
+        return item->artistId;
+    } else if(role == Qt::UserRole+2) {
+        return item->artistName;
+    } else if(role == Qt::UserRole+3) {
+        return item->artistCover;
+    } else if(role == Qt::UserRole+4) {
+        return item->albumCoverId;
+    } else if(role == Qt::UserRole+5) {
+        return item->albumName;
+    } else if (role == Qt::UserRole+6) {
+        return item->albumCover;
+    } else if (role == Qt::UserRole+7) {
+        return item->trackName;
+    } else if (role == Qt::UserRole+8) {
+        return item->type;
+    } else if (role == Qt::UserRole+9) {
+        return item->duration;
+    } else if (role == Qt::UserRole+10) {
