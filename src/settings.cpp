398ce3e7a1079953f981fc276bf40164631d2c26
seejinmax <123712248+seejinmax@users.noreply.github.com>
2023-02-12 09:50:09 +0000

    Tag

--- src/settings.cpp
+++ src/settings.cpp
@@ -1,4 +1,4 @@
 #include "settings.h"
 
 Settings::Settings(QObject *parent): QSettings(QStandardPaths::writableLocation(QStandardPaths::ConfigLocation) +
-                      
+                                               "/org.ilyavysotsky/yasailmusic/yasailmusi
